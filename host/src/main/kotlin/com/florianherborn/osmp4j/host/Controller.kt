c4c854fd1e858ac47e14deabd64042a92d10ff5a
Florian Herborn <florianherborn2293@t-online.de>
2019-08-01 16:34:23 +0200

    Test Controller umbenannt in Controller

--- host/src/main/kotlin/com/florianherborn/osmp4j/host/Controller.kt
+++ host/src/main/kotlin/com/florianherborn/osmp4j/host/Controller.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/")
-class TestController @Autowired constructor(private val preparationService: PreparationService, private val duplicatesService: DuplicatesService) {
+class Controller @Autowired constructor(private val preparationService: PreparationService, private val duplicatesService: DuplicatesService) {
 
     @GetMapping
     fun send() = preparationService.prepateBoundingBox()
