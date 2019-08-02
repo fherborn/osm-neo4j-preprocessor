2da91e289d594eba96954f5e47f60c2d14cbf620
fherborn <florianherborn2293@t-online.de>
2019-08-02 14:42:01 +0200

    Eingabeinterface implementiert

--- host/src/main/kotlin/com/osmp4j/host/InterfaceController.kt
+++ host/src/main/kotlin/com/osmp4j/host/InterfaceController.kt
@@ -3,15 +3,62 @@ package com.osmp4j.host
 import com.osmp4j.host.rabbit.DuplicatesService
 import com.osmp4j.host.rabbit.PreparationService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import javax.validation.Valid
 
-@RestController
-@RequestMapping("/")
+
+data class ExportForm(
+
+        var taskName: String = "Task-${UUID.randomUUID()}",
+
+        var fromLat: Double = 7.0862,
+
+        var fromLon: Double = 51.0138,
+
+        var toLat: Double = 7.7344,
+
+        var toLon: Double = 51.3134
+
+)
+
+@Controller
+@RequestMapping
 class InterfaceController @Autowired constructor(private val preparationService: PreparationService, private val duplicatesService: DuplicatesService) {
 
+//    @GetMapping
+//    fun send() = preparationService.prepareBoundingBox()
+
+/*
+    Example to fill html model
+    @GetMapping("/export")
+    fun export(@RequestParam(name = "name", required = false, defaultValue = "World") name: String, model: Model): String {
+        model.addAttribute("name", name)
+        return "index"
+    }
+*/
+
+
+//    @GetMapping("/export")
+//    fun greetingForm(model: Model): String {
+//        model.addAttribute("export", ExportForm())
+//        return "export"
+//    }
+
     @GetMapping
-    fun send() = preparationService.prepareBoundingBox()
+    fun export(model: Model): String {
+        model.addAttribute("exportForm", ExportForm())
+        return "index"
+    }
+
+    @PostMapping
+    fun greetingSubmit(@Valid @ModelAttribute("exportForm") exportForm: ExportForm, model: Model): String {
+        return "result"
+    }
 
 }
