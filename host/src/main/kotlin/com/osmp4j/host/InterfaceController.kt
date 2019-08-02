d838e7aea2b280c0b21f127f00494cf506388cfa
fherborn <florianherborn2293@t-online.de>
2019-08-02 12:11:42 +0200

    Rabbit Services gemoved zu rabbit/...

--- host/src/main/kotlin/com/osmp4j/host/InterfaceController.kt
+++ host/src/main/kotlin/com/osmp4j/host/InterfaceController.kt
@@ -1,5 +1,7 @@
 package com.osmp4j.host
 
+import com.osmp4j.host.rabbit.DuplicatesService
+import com.osmp4j.host.rabbit.PreparationService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
