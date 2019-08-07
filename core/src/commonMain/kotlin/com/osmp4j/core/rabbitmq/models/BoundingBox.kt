538615644f3234b633331a6d20e718d4e8e589cf
fherborn <florianherborn2293@t-online.de>
2019-08-07 13:57:28 +0200

    Geo Utils hinzugefügt und Bounding Box erweitert um Größenberechnung

--- core/src/commonMain/kotlin/com/osmp4j/core/rabbitmq/models/BoundingBox.kt
+++ core/src/commonMain/kotlin/com/osmp4j/core/rabbitmq/models/BoundingBox.kt
@@ -1,3 +1,11 @@
 package com.osmp4j.core.rabbitmq.models
 
-data class BoundingBox(var fromLat: Double, var fromLon: Double, var toLat: Double, var toLon: Double)
+import com.osmp4j.core.distanceTo
+import com.osmp4j.core.toRadiant
+
+data class BoundingBox(var fromLat: Double, var fromLon: Double, var toLat: Double, var toLon: Double) {
+    fun widthDegree() = fromLon distanceTo toLon
+    fun heightDegree() = fromLat distanceTo toLat
+    fun withRadiant() = widthDegree().toRadiant()
+    fun heightRadiant() = heightDegree().toRadiant()
+}
