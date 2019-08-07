9d1cf24c93bec9f881cab84d3b95cc29eafad7ad
fherborn <florianherborn2293@t-online.de>
2019-08-07 16:36:40 +0200

    Message und services angepasst

--- agent/src/main/kotlin/com/osmp4j/agent/PreparationService.kt
+++ agent/src/main/kotlin/com/osmp4j/agent/PreparationService.kt
@@ -1,6 +1,8 @@
 package com.osmp4j.agent
 
 import com.osmp4j.core.rabbitmq.QueueNames
+import com.osmp4j.core.rabbitmq.models.PreparationRequest
+import com.osmp4j.core.rabbitmq.models.Success
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,9 +12,9 @@ import org.springframework.stereotype.Service
 class PreparationService @Autowired constructor(private val template: RabbitTemplate) {
 
     @RabbitListener(queues = [QueueNames.REQUEST_PREPARATION])
-    fun onPreparationRequest(message: String) {
-        println("Agent: Preparation Request -> $message")
-        template.convertAndSend(QueueNames.RESPONSE_PREPARATION, "Preparation done")
+    fun onPreparationRequest(request: PreparationRequest) {
+        println("Agent: Preparation Request -> $request")
+        template.convertAndSend(QueueNames.RESPONSE_PREPARATION, Success("filename", request.id))
     }
 
 }
