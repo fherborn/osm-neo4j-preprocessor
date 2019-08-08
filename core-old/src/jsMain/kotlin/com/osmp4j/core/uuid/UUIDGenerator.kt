776e10b53449913c0d8cdb876886f0de80c1133f
fherborn <florianherborn2293@t-online.de>
2019-08-08 07:24:29 +0200

    Alter core gelöscht

--- core-old/src/jsMain/kotlin/com/osmp4j/core/uuid/UUIDGenerator.kt
+++
@@ -1,7 +0,0 @@
-package com.osmp4j.core.uuid
-
-actual object UUIDGenerator {
-    actual fun randomUUID(): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
--- core-old/src/jvmMain/kotlin/com/osmp4j/core/uuid/UUIDGenerator.kt
+++
@@ -1,7 +0,0 @@
-package com.osmp4j.core.uuid
-
-import java.util.*
-
-actual object UUIDGenerator {
-    actual fun randomUUID(): String = UUID.randomUUID().toString()
-}
