cbacff2defb04c131fb9eb7d2a2e60c2447b5169
fherborn <florianherborn2293@t-online.de>
2019-08-09 12:59:26 +0200

    Erster versuch parsing

--- agent/src/main/kotlin/com/osmp4j/agent/models/Models.kt
+++ agent/src/main/kotlin/com/osmp4j/agent/models/Models.kt
@@ -1,72 +1,180 @@
 package com.osmp4j.agent.models
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
+import com.osmp4j.agent.noarg.NoArg
 
+@NoArg
 data class OsmFile(
-        val osmRoot: OsmRoot
+        val osm: OsmRoot
 )
 
+@NoArg
+@JacksonXmlRootElement(localName = "osm")
 data class OsmRoot(
+
+        @JacksonXmlProperty(isAttribute = true)
         val version: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         val generator: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         val copyright: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         val attribution: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         val license: String,
+
+        @JacksonXmlProperty(isAttribute = false)
         val bounds: Bounds,
-        val nodes: Sequence<Node>,
-        val ways: Sequence<Way>,
-        val relations: Sequence<Relation>
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        val node: Sequence<Node>,
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        val way: Sequence<Way>,
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        val relation: Sequence<Relation>
 )
+
+@NoArg
 data class Bounds (
+
+        @JacksonXmlProperty(isAttribute = true)
         val minlat: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         val minlon: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         val maxlat: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         val maxlon: String
 )
 
+@NoArg
 data class Node(
+
+        @JacksonXmlProperty(isAttribute = true)
         val lat: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         val lon: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val id: Long,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val visible: Boolean,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val version: Int,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val changeset: Long,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val timestamp: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val user: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val uid: String,
-        override val tags: HashMap<String, String>
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        override val tag: HashMap<String, String>
 ): OsmEntity
 
+@NoArg
 data class Relation(
+
+        @JacksonXmlProperty(isAttribute = true)
         override val id: Long,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val visible: Boolean,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val version: Int,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val changeset: Long,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val timestamp: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val user: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val uid: String,
-        override val tags: HashMap<String, String>,
-        val members: Sequence<Member>
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        override val tag: HashMap<String, String>,
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        val member: Sequence<Member>
 ): OsmEntity
 
+@NoArg
 data class Member(
+
+        @JacksonXmlProperty(isAttribute = true)
         val type: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         val role: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val ref: Long
 ): Ref
 
+@NoArg
 data class Way(
+
+        @JacksonXmlElementWrapper(useWrapping = false)
         val nd: Sequence<NodeRef>,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val id: Long,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val visible: Boolean,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val version: Int,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val changeset: Long,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val timestamp: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val user: String,
+
+        @JacksonXmlProperty(isAttribute = true)
         override val uid: String,
-        override val tags: HashMap<String, String>
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        @JsonIgnore
+        override val tag: HashMap<String, String> = hashMapOf()
 ): OsmEntity
 
-data class NodeRef(override val ref: Long): Ref
+@NoArg
+data class NodeRef(
+
+        @JacksonXmlProperty(isAttribute = true)
+        override val ref: Long
+) : Ref
 
 
 interface OsmEntity {
@@ -77,7 +185,7 @@ interface OsmEntity {
     val timestamp: String
     val user: String
     val uid: String
-    val tags: HashMap<String, String>
+        val tag: HashMap<String, String>
 }
 
 interface Ref {
--- agent/src/main/kotlin/com/osmp4j/agent/services/PreparationService.kt
+++ agent/src/main/kotlin/com/osmp4j/agent/services/PreparationService.kt
@@ -1,11 +1,18 @@
 package com.osmp4j.agent.services
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
+import com.osmp4j.agent.models.OsmRoot
 import com.osmp4j.ftp.FTPService
 import com.osmp4j.http.HttpService
 import com.osmp4j.messages.BoundingBoxToLargeError
+import com.osmp4j.messages.PreparationError
 import com.osmp4j.messages.PreparationRequest
 import com.osmp4j.messages.PreparationResponse
-import com.osmp4j.mq.*
+import com.osmp4j.mq.BoundingBox
+import com.osmp4j.mq.QueueNames
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.amqp.rabbit.core.RabbitTemplate
@@ -14,7 +21,8 @@ import org.springframework.stereotype.Service
 import java.io.File
 import java.util.*
 
-fun File.readFirstLine() = bufferedReader().use { it.readLine() }
+
+fun File.readFirstLine(): String = bufferedReader().use { it.readLine() }
 
 private const val BOX_TO_LARGE_ERROR_START = "You requested too many nodes"
 
@@ -35,43 +43,58 @@ class PreparationService @Autowired constructor(
         logger.debug("Received request with ID: ${request.id}, BoundingBox: ${request.boundingBox}")
 
         val boundingBox = request.boundingBox
-        val url = getUrl(boundingBox)
 
-        val fileName = "${UUID.randomUUID()}.txt"
-        val file = httpService.download(url, fileName)
+        val file = download(boundingBox)
 
         logger.debug("Started checking for error")
 
-        val potentialError = file.readFirstLine()
-
-        if (potentialError.startsWith(BOX_TO_LARGE_ERROR_START)) {
-            logger.debug("Box to large, sending error to host.")
-            template.convertAndSend(QueueNames.PREPARATION_ERROR, BoundingBoxToLargeError(boundingBox, request.id))
-        } else {
-            logger.debug("No Errors found.")
-            //TODO start preprocessing
-            upload(fileName, file)
-            sendResultToHost(fileName, request)
+        when {
+            file.readFirstLine().startsWith(BOX_TO_LARGE_ERROR_START) -> boxToLargeErrorFound(boundingBox, request)
+            else -> startPreparing(file, request)
         }
+    }
 
+    private fun startPreparing(rawFile: File, request: PreparationRequest) {
+
+        val mapper = XmlMapper()
+        mapper.registerModule(ParameterNamesModule())
+        mapper.registerModule(KotlinModule())
+        val osmFile = mapper.readValue<OsmRoot>(rawFile.inputStream())
+        logger.debug("Node count: ${osmFile.node.count()}")
 
         logger.debug("Deleting local file")
-        file.delete()
-
-
+        rawFile.delete()
+        publish(rawFile, request)
     }
+
+    private fun boxToLargeErrorFound(boundingBox: BoundingBox, request: PreparationRequest) {
+        logger.debug("Box to large, sending error to host.")
+        sendError(BoundingBoxToLargeError(boundingBox, request.id))
+    }
+
+    private fun sendError(error: PreparationError) = template.convertAndSend(QueueNames.PREPARATION_ERROR, error)
+
+    private fun download(boundingBox: BoundingBox) =
+            httpService.download(getUrl(boundingBox), "${UUID.randomUUID()}.txt")
+
 
     private fun getUrl(boundingBox: BoundingBox) =
             "https://www.openstreetmap.org/api/0.6/map?bbox=${boundingBox.fromLat},${boundingBox.fromLon},${boundingBox.toLat},${boundingBox.toLon}"
 
-    private fun sendResultToHost(fileName: String, request: PreparationRequest) {
-        template.convertAndSend(QueueNames.PREPARATION_RESPONSE, PreparationResponse(fileName, request.id))
+    private fun publish(file: File, request: PreparationRequest) {
+
+        //TODO each agent own folder
+        logger.debug("Started uploading")
+        ftpService.upload(file.name, file)
+        logger.debug("Finished uploading")
+
+        logger.debug("Started sending to host")
+        sendResultToHost(file.name, request)
+        logger.debug("Finished sending to host")
     }
 
-    private fun upload(fileName: String, file: File) {
-        logger.debug("Started uploading")
-        ftpService.upload(fileName, file)
-        logger.debug("Finished uploading")
+    private fun sendResultToHost(fileName: String, request: PreparationRequest) {
+        template.convertAndSend(QueueNames.PREPARATION_RESPONSE, PreparationResponse(fileName, request.id))
     }
 
 }
